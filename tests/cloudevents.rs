9606ce60ae58c289d96551cf281a94b07262731f
Kevin Hoffman <alothien@gmail.com>
2018-05-08 16:07:00 -0400

    cloud events test

--- tests/cloudevents.rs
+++ tests/cloudevents.rs
@@ -20,7 +20,7 @@ enum TestEvent {
 }
 
 #[test]
-fn cloud_event_roundtrip() {
+    fn cloud_event_roundtrip() {
     // ensure that we can produce a cloud event with an arbitrary nested JSON value in the data
     // field and serialize it, then deserialize it, and still be able to convert the serde_json::Value
     // back into the original raw event type.
