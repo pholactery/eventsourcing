79cf6be1a44f3918fc04c5db88662e84044b7177
Kevin Hoffman <alothien@gmail.com>
2020-02-04 09:23:34 -0500

    Upgrading cloud events support to the 1.0 version of the JSON format

--- src/cloudevents.rs
+++ src/cloudevents.rs
@@ -1,8 +1,8 @@
 //! Cloud Events Implementation
 //!
 //! This module provides an implementation of a [cloud event](https://cloudevents.io/)
-//! data structure that conforms to the cloud events v0.1 spec as outlined in
-//! [CloudEvents v0.1 Spec](https://github.com/cloudevents/spec/blob/v0.1/spec.md)
+//! data structure that conforms to the cloud events v1.0 spec as outlined in
+//! [CloudEvents v1.0 Spec](https://github.com/cloudevents/spec/blob/v1.0/spec.md)
 //!
 //! In the current version of this library, only the _application/json_ content type is supported
 //! for the `data` field on the cloud event.
@@ -11,18 +11,23 @@ use chrono::prelude::*;
 use serde_json;
 use uuid::Uuid;
 
-/// CloudEvent provides a data structure that is JSON-compliant with v0.1 of the CloudEvents
+/// CloudEvent provides a data structure that is JSON-compliant with v1.0 of the CloudEvents
 /// specification. This means that any system with which you want to communicate that is
 /// also CloudEvents-aware can accept the serialized version of this data structure.
 #[derive(Debug, Serialize, Deserialize, Clone)]
-#[serde(rename_all = "camelCase")]
 pub struct CloudEvent {
+    #[serde(rename = "specversion")]
     pub cloud_events_version: String,
+    #[serde(rename = "type")]
     pub event_type: String,
+    #[serde(rename = "typeversion")]
     pub event_type_version: String,
     pub source: String, // URI
+    #[serde(rename = "id")]
     pub event_id: String,
+    #[serde(rename = "time")]
     pub event_time: DateTime<Utc>,
+    #[serde(rename = "datacontenttype")]
     pub content_type: String,
     pub data: serde_json::Value,
 }
@@ -35,7 +40,7 @@ where
         let raw_data = serde_json::to_string(&source).unwrap();
 
         CloudEvent {
-            cloud_events_version: "0.1".to_owned(),
+            cloud_events_version: "1.0".to_owned(),
             event_type: source.event_type().to_owned(),
             event_type_version: source.event_type_version().to_owned(),
             source: source.event_source().to_owned(),
